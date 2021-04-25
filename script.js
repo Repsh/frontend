38998eccb73ab8febae9649ea2fb47c734104327
Repsh <76905614+Repsh@users.noreply.github.com>
2021-04-25 21:43:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function displayAvatar(form, response) {
-   let app = document.querySelector('#app');
-   let output = '';
+    let app = document.querySelector('#app');
+    let output = ''
+   
 
-   for ( entry in response) {
-       console.log(response[entry]);
-       if ( entry !== 'image') {
-            output += '<p><b>' + entry + '</b>' + response[entry] + "</p>";
-       }
-       else {
-           output += '<img scr=""' + response[entry] + '"height="100px" >';
-       }
-       
-   }
+    for (entry in response) {
+        console.log(response[entry]);
+        if (entry !== 'image') {
+            output += '<p><b>' + entry + '</b> ' + response[entry] + "</p>"
+        }
+        else {
+            output += '<img src="' + response[entry] + '" height="100px" >';
+        }
+    }
 
-   app.innerHTML = output;
+
+    app.innerHTML = output;
+
 }
